0a160db1e0decc56af6c42a3bc86837eb7eaf7e5
RAO <vishalrao.bandagar@citiustech.com>
2022-02-02 17:48:11 +0530

    added routing for login component

--- patient-portal/src/app/app-routing.module.ts
+++ patient-portal/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) }, { path: 'physician', loadChildren: () => import('./physician/physician.module').then(m => m.PhysicianModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'header', loadChildren: () => import('./header/header.module').then(m => m.HeaderModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: 'patient',
+    loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule)
+  },
+  {
+    path: 'physician',
+    loadChildren: () => import('./physician/physician.module').then(m => m.PhysicianModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'header',
+    loadChildren: () => import('./header/header.module').then(m => m.HeaderModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- patient-portal/src/app/auth/login/login.component.ts
+++ patient-portal/src/app/auth/login/login.component.ts
@@ -17,9 +17,15 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: [null, [Validators.required, Validators.minLength(6)]],
+      password: [null, [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
     });
   }
+
+  // Validators.pattern('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$')
+
+  // Minimum eight characters, at least one letter and one number
+
+
 
   loginDetails(formData:any) {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(formData.value, null, 4));
