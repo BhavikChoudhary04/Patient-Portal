c6d3313d0989ddf7f844e6d48c90b8c45b269e33
Mayur Raina <MayurR2@citiustech.com>
2022-02-02 11:28:21 +0530

    reset input issue

--- patient-portal/src/app/auth/auth-routing.module.ts
+++ patient-portal/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { ResetComponent } from './reset/reset.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'reset', component: ResetComponent },
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- patient-portal/src/app/auth/auth.module.ts
+++ patient-portal/src/app/auth/auth.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 import { AuthComponent } from './auth.component';
+import { ResetComponent } from './reset/reset.component';
+
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, ResetComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatCardModule,
+    MatFormFieldModule
   ]
 })
 export class AuthModule { }
