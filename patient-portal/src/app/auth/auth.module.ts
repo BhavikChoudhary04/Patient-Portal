af0987844442dad5aae8126107c31c7d0ddabbab
Preeti Mondal <Preeti.Mondal@citiustech.com>
2022-02-02 12:29:54 +0530

    login form

--- patient-portal/src/app/auth/auth.module.ts
+++ patient-portal/src/app/auth/auth.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
+  exports: [LoginComponent],
+  providers: [],
 })
 export class AuthModule { }
