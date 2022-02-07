e61cd14c67338bd1f979dac7852ac890449a611a
RAO <vishalrao.bandagar@citiustech.com>
2022-02-07 11:00:27 +0530

    added validation for DOB

--- patient-portal/src/app/auth/register/register.component.ts
+++ patient-portal/src/app/auth/register/register.component.ts
@@ -1,8 +1,9 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 export const MY_FORMATS = {
   parse: {
@@ -30,23 +31,44 @@ export class RegisterComponent implements OnInit {
   registerForm!: FormGroup
   flag: boolean = true;
   CPflag: boolean = true;
-
+  minDate! : any;
+  maxDate! : any;
+  
   constructor(
     private fb:FormBuilder,
     private router:Router,
     private datePipe: DatePipe
     ) {
       this.createRegisterForm();
-     }
+      this.minDate = moment().subtract(120, 'years');
+      this.minDate = new Date (this.minDate);
+      this.maxDate =  new Date();
+    }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+
+  } 
+
+  confirmedValidator(controlName: string, matchingControlName: string){
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ confirmedValidator: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
 
   createRegisterForm(){
     this.registerForm = this.fb.group({
       firstName : ['', [Validators.required,Validators.maxLength(20), Validators.pattern('^[a-zA-Z ]*$')]],
       lastName : ['', [Validators.required,Validators.maxLength(20), Validators.pattern('^[a-zA-Z ]*$')]],
-      dob : ['', Validators.required],
+      dob : ['', [ Validators.required]],
       userName : ['', Validators.required],
       role : ['', Validators.required],
       email : ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
@@ -54,7 +76,8 @@ export class RegisterComponent implements OnInit {
       // mobile : ['', [Validators.required, Validators.minLength(10), 
       //               Validators.maxLength(10),Validators.pattern("^((\\+91-?)|7)?[0-9]{10}$")]],
       mobile : ['', [Validators.required, Validators.minLength(10), 
-                     Validators.maxLength(10),Validators.pattern("^[6-9][0-9]+")]],
+                     Validators.maxLength(10),Validators.pattern("^[6-9][0-9]+")],
+                     Validators.maxLength(10)],
                     // number starting from 6-9 and containing 10 digits
       password : ['', [Validators.required, Validators.minLength(8), 
                        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
@@ -63,13 +86,16 @@ export class RegisterComponent implements OnInit {
                                Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]]
                       // Minimum eight characters, at least one upercase letter, one lowercase letter, one special character and one number
 
+    },{ 
+      validator: this.confirmedValidator('password', 'confirmPassword')
     })
   }
 
   submitRegisterForm(){
-    if (this.registerForm.invalid) return
-      this.router.navigateByUrl('/auth/login');
+     if (this.registerForm.invalid) return
+     
       this.registerForm.value.dob = this.datePipe.transform(this.registerForm.value.dob, 'dd-MM-yyyy');
+      this.router.navigateByUrl('/auth/login');
       //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
       console.log(this.registerForm.value);
     }
