26a0e064245c107586f109e19ef6f007da893f4a
RAO <vishalrao.bandagar@citiustech.com>
2022-02-15 20:17:58 +0530

    logout functionality integration

--- patient-portal/src/app/patient/dashboard/dashboard.component.ts
+++ patient-portal/src/app/patient/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public matDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openLogoutModal() {
+    const dialogConfig = new MatDialogConfig();
+    // The user can't close the dialog by clicking outside its body
+    dialogConfig.disableClose = true;
+    dialogConfig.id = "modal-component";
+    dialogConfig.height = "180px";
+    dialogConfig.width = "350px";
+    dialogConfig.data = {
+      name: "logout",
+      title: "Are you sure you want to logout?",
+      // description: ":)",
+      actionButtonText: "Logout"
+    }
+    const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
+  }
 }
