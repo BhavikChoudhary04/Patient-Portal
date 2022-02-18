b33854e4b613314e2779b4c778c35122eac4caf1
Bhavik Choudhary <BhavikC@citiustech.com>
2022-02-18 14:58:28 +0530

    dropdown changes

--- patient-portal/src/app/patient/schedule-appointment/schedule-appointment.component.ts
+++ patient-portal/src/app/patient/schedule-appointment/schedule-appointment.component.ts
@@ -14,6 +14,8 @@ export class ScheduleAppointmentComponent implements OnInit {
 
   physicianForm!: FormGroup
   patientData: any;
+  showDetails:boolean =false
+
 
   constructor(
     private fb:FormBuilder,
@@ -24,71 +26,13 @@ export class ScheduleAppointmentComponent implements OnInit {
       this.createPhysicianForm();
    }
 
-  //  data =[ {
-  //   "name": "Harry",
-  //   "availableDate": [
-  //     "20/02/2022",
-  //     "09/03/2022",
-  //     "11/03/2022",
-  //     "20/03/2022"
-  //   ],
-  //   "availabletime": [
-  //     "09:00 AM",
-  //     "11:00 AM",
-  //     "02:00 PM",
-  //     "05:00 PM"
-  //   ]
-  // },
-  // {
-  //   "name": "Matt Murdock",
-  //   "availableDate": [
-  //     "21/02/2022",
-  //     "03/03/2022",
-  //     "10/03/2022",
-  //     "17/03/2022"
-  //   ],
-  //   "availabletime": [
-  //     "10:00 AM",
-  //     "12:00 PM",
-  //     "02:00 PM",
-  //     "06:00 PM"
-  //   ]
-  // },
-  // {
-  //   "name": "Frank Castle",
-  //   "availableDate": [
-  //     "23/02/2022",
-  //     "08/03/2022",
-  //     "11/03/2022",
-  //     "19/03/2022"
-  //   ],
-  //   "availabletime": [
-  //     "08:00 AM",
-  //     "11:00 AM",
-  //     "01:00 PM",
-  //     "04:00 PM"
-  //   ]
-  // },
-  // {
-  //   "name": "Bruce Banner",
-  //   "availableDate": [
-  //     "25/02/2022",
-  //     "27/02/2022",
-  //     "01/03/2022",
-  //     "15/03/2022"
-  //   ],
-  //   "availabletime": [
-  //     "09:00 AM",
-  //     "10:30 AM",
-  //     "04:00 PM",
-  //     "07:00 PM"
-  //   ]
-  // }]
-
   ngOnInit(): void {
     this.appointments.getPhysician().subscribe(data => {
-      console.log(data);
-      this.patientData = data;
+      if(data){
+        console.log(data);
+        this.patientData = data;
+        this.showDetails = true
+      }
     })
   }
 
