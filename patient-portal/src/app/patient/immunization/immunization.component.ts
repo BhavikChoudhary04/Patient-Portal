0add86cb30937fdee838f848eb03a412670beffc
RAO <vishalrao.bandagar@citiustech.com>
2022-02-22 08:36:10 +0530

    added delete and edit icon

--- patient-portal/src/app/patient/immunization/immunization.component.ts
+++ patient-portal/src/app/patient/immunization/immunization.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { DemographicService } from 'src/app/services/demographic.service';
+import { ImmunizationService } from 'src/app/services/immunization.service';
 import { ImmunizationDialogComponent } from '../components/immunization-dialog/immunization-dialog.component';
 
 @Component({
@@ -12,7 +13,12 @@ import { ImmunizationDialogComponent } from '../components/immunization-dialog/i
 
 export class ImmunizationComponent implements OnInit {
 
-  showDemographicDetails: boolean = false
+  showDemographicDetails: boolean = false;
+  displayedColumns: string[] = ['name', 'doses', 'lastDoseDate', 'edit', 'delete'];
+  otherVaccineColumns: string[] = ['name', 'doses', 'lastDoseDate', 'edit', 'delete'];
+  covidVaccinationDetails !:string[];
+  otherVaccinationDetails !:string[];
+
   userDetailFields:[{vaccineName:string, vaccineNumberOfDoses: string, dateOfLastVaccine: string, anyOtherVaccines: string}]= [
     {
       vaccineName: 'Covishield',
@@ -35,10 +41,63 @@ export class ImmunizationComponent implements OnInit {
     }
   ]
 
-  constructor(private fb:FormBuilder, private demoService:DemographicService, public dialog: MatDialog) {}
+  constructor(private immunizationService: ImmunizationService, private fb:FormBuilder, private demoService:DemographicService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    let sessionUser:any = sessionStorage.getItem('user');
+
+    if (sessionUser){
+      sessionUser = JSON.parse(sessionUser);
+      console.log("immunization sessionUser--->",sessionUser)
+      this.immunizationService.getImmunizationDetails(sessionUser!.id).then( x =>{
+        this.getUserImmunizationDetail();
+      })
+    }
+
   }
+
+  // ngAfterViewInit(){
+  //   this.getUserImmunizationDetail();
+  // }
+
+  getUserImmunizationDetail(){
+    this.immunizationService.getImmunizations().subscribe((response:any)=>{
+      // console.log("report details of logged in user:- ",response);
+      if(response){
+        console.log("response--->",response)
+
+        let allVaccinesDetails = response[0].vaccines;
+        
+        this.covidVaccinationDetails= allVaccinesDetails.filter((record:any) => {
+            return record.type === 'COVID';
+        });
+
+        this.otherVaccinationDetails= allVaccinesDetails.filter((record:any) => {
+          return record.type === 'other';
+      });
+        console.log("this.covidVaccinationDetails--->",this.covidVaccinationDetails)
+
+        console.log("this.otherVaccinationDetails--->",this.otherVaccinationDetails)
+      }
+    })
+  }
+
+  editCovidVaccineDetails(row:any){
+
+  }
+
+  deleteCovidVaccineRecord(row:any){
+
+  }
+
+  editOtherVaccineDetails(row:any){
+
+  }
+
+  deleteOtherVaccineRecord(row:any){
+
+  }
+
   openEditDemoDialog(): void{
     const dialogRef = this.dialog.open(ImmunizationDialogComponent, {
       width: '60%',
