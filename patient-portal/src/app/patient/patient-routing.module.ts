c5899e0c07bd7c23d6f77b75497c75bd55264b9f
RAO <vishalrao.bandagar@citiustech.com>
2022-02-22 11:30:34 +0530

    updated changes

--- patient-portal/src/app/patient/patient-routing.module.ts
+++ patient-portal/src/app/patient/patient-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     {
       path: 'dashboard',
       component: PatientComponent,
-    // canActivateChild: [PatientGuard],
+      canActivateChild: [PatientGuard],
     children: [
       {
         path:'my-profile',
